02e437a62f50ef203fb0ed7bfdc2e9f66ae5788f
santosh.bukkashetti <santosh.bukkashetti@CA-L6PBSQQ2.groupinfra.com>
2019-08-14 15:50:58 -0400

    Resetting App.java

--- src/main/java/trials/git/App.java
+++ src/main/java/trials/git/App.java
@@ -9,10 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello Git!" );
-        System.out.println( "Hello unknown!" );
-        System.out.println( "Hello conflict!" );
-        System.out.println( "Hello conflictsecondTime!" );
-        System.out.println( "Hello sickKinds!" );
     }
 }
