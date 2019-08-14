1d3237aa11c68565f112cc08f750c32ef4e3847c
santosh.bukkashetti <santosh.bukkashetti@CA-L6PBSQQ2.groupinfra.com>
2019-08-14 12:51:39 -0400

    resolved conflicts with conflict vs unknown.

--- src/main/java/trials/git/App.java
+++ src/main/java/trials/git/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello Git!" );
+        System.out.println( "Hello unknown!" );
+        System.out.println( "Hello conflict!" );
     }
 }
