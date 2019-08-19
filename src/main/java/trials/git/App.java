504258114efdb24dde42753fd985a5704821bab4
Bukkashetti <santosh.bukkashetti@cgi.com>
2019-08-19 15:30:24 -0400

    one more conflict please

--- src/main/java/trials/git/App.java
+++ src/main/java/trials/git/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello OtherText!" );
+        System.out.println("I want to keep this");
     }
 }
