488e295a473bcd3641e278cadf6bf55c3c9ea65a
santosh.bukkashetti <santosh.bukkashetti@CA-L6PBSQQ2.groupinfra.com>
2019-08-19 15:34:24 -0400

    Resolved

--- src/main/java/trials/git/App.java
+++ src/main/java/trials/git/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello OtherText!" );
+        System.out.println( "Let's keep this this-time!" );
     }
 }
